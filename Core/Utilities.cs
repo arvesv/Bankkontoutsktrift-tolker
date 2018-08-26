1f6fa98d6d3a36355b575ef5f2cddf408453df1c
Arve Svendsen <657470+arvesv@users.noreply.github.com>
2018-08-26 14:08:05 +0200

    A better accoutdiff

--- Core/Utilities.cs
+++ Core/Utilities.cs
@@ -50,28 +50,49 @@ namespace Core
                        .GetTransactions() ?? new List<Transaction>();
         }
 
-        public static bool IsEqualTrans(Transaction t1, Transaction t2)
-        {
-            const int maxDateDiff = 4;
-
-            return t1.Amount == t2.Amount && (t1.TransactionDate - t2.TransactionDate).Days < maxDateDiff;
-        }
-
-        private static IList<T> OnlyInFirstList<T>(IEnumerable<T> first, IList<T> second, Equals<T> comparer)
-        {
-            return (from t in first
-                let potentialMatches =
-                    second.Where(s => comparer(s, t))
-                where !potentialMatches.Any()
-                select t).ToList();
-        }
-
         public static (IList<Transaction> onlyFirst, IList<Transaction> onlySecond) CompareListe(
             IList<Transaction> first, IList<Transaction> second)
         {
-            return (
-                OnlyInFirstList(first, second, IsEqualTrans),
-                OnlyInFirstList(second, first, IsEqualTrans));
+            IList<Transaction> onlyFirst = new List<Transaction>();
+            bool[] alreadyMatchedList = new bool[second.Count];
+
+            foreach (var firstLisTransaction in first)
+            {
+                int? bestMatch = null;
+                var mindatediff = 0;
+
+
+                for (int pos = 0; pos < second.Count; pos++)
+                {
+                    if (alreadyMatchedList[pos] || firstLisTransaction.Amount != second[pos].Amount) continue;
+
+                    var datediff = Math.Abs((firstLisTransaction.TransactionDate - second[pos].TransactionDate).Days);
+                    if(datediff > 3)
+                        continue;
+
+                    if (bestMatch == null || datediff < mindatediff)
+                    {
+                        mindatediff = bestMatch ?? Math.Min(mindatediff, datediff) | datediff;
+                        bestMatch = pos;
+                    }
+                }
+
+                if (bestMatch.HasValue)
+                {
+                    alreadyMatchedList[bestMatch.Value] = true;
+                }
+                else
+                {
+                    onlyFirst.Add(firstLisTransaction);
+                }
+            }
+
+            var onlySecond = Enumerable.Range(0, second.Count)
+                .Where(pos => !alreadyMatchedList[pos])
+                .Select(pos => second[pos])
+                .ToList();
+
+            return (onlyFirst, onlySecond);
         }
     }
 }
